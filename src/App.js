f661bdcf324d17d00de0e5affd658ae2a5bc589e
jaseongKim <jaseong4281@gmail.com>
2022-08-30 10:30:58 +0900

    add template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import GlobalStyle from "./shared/GlobalStyle";
+import Router from "./shared/Router";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
+}
+
+export default App;
