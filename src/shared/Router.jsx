0fc35ad674ecde0bd951e7768b2eebef3db9c590
jaseongKim <jaseong4281@gmail.com>
2022-10-01 20:51:21 +0900

    [ refactor ] import logoPageFix

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -20,6 +20,7 @@ import MaintPage from "../pages/MaintPage";
 
 // Google Analytics
 import ReactGA from "react-ga";
+import LogoPageFix from "../pages/LogoPageFix";
 
 const TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS; // MEASUREMENT ID
 ReactGA.initialize(TRACKING_ID);
