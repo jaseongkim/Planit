15ee85c6ca2f149ccc9575a8fc3405c6eef911ac
ParkJong-ic <whddlrs@naver.com>
2022-10-03 21:10:41 +0900

    fix: timer 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -19,37 +19,10 @@ import NickName from "../components/mypage/NickName";
 import Password from "../components/mypage/Password";
 import NotFoundPage from "../pages/NotFoundPage";
 import MaintPage from "../pages/MaintPage";
-<<<<<<< HEAD
 import Statistic from "../pages/Statistic";
-=======
-import LogoPageFix from "../pages/LogoPageFix";
->>>>>>> f3b01ab9980bafddedc85564217f4e1b163928c0
 
 const Router = () => {
   return (
-<<<<<<< HEAD
-      <Routes>
-        <Route path="/" element={<LogoPage />} />
-        <Route path="/notfound" element={<NotFoundPage />} />
-        <Route path="/maintPage" element={<MaintPage />} />
-        <Route path="/login" element={<LogInPage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/dlytodo" element={<DlyTodo />} />
-        <Route path="/wklytodo" element={<WklyTodo />} />
-        <Route path="/login/kakao" element={<Kakao />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/follow" element={<Follow />} />
-        <Route path="/createplanet" element={<CreatePlanet />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/nickname" element={<NickName />} />
-        <Route path="/password" element={<Password />} />
-        <Route path="/category" element={<Category />} />
-        <Route path="/categorydetail/:id" element={<CategoryDetail />} />
-        <Route path="*" element={<LogoPageFix />} />
-        <Route path="/statistic" element={<Statistic />} />
-      </Routes>
-=======
     <Routes>
       <Route path="/" element={<LogoPage />} />
       <Route path="/notfound" element={<NotFoundPage />} />
@@ -63,17 +36,16 @@ const Router = () => {
       <Route path="/search" element={<Search />} />
       <Route path="/follow" element={<Follow />} />
       <Route path="/createplanet" element={<CreatePlanet />} />
-      <Route path="/timerpage" element={<TimerPage />} />
       <Route path="/mypage" element={<MyPage />} />
       <Route path="/nickname" element={<NickName />} />
       <Route path="/password" element={<Password />} />
       <Route path="/category" element={<Category />} />
       <Route path="/categorydetail/:id" element={<CategoryDetail />} />
       <Route path="*" element={<LogoPageFix />} />
+      <Route path="/timer" element={<TimerPage />} />
+      <Route path="/statistic" element={<Statistic />} />
     </Routes>
->>>>>>> f3b01ab9980bafddedc85564217f4e1b163928c0
   );
 };
 
 export default Router;
-
