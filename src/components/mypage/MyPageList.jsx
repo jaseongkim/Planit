079da68d276f8dd8d6d5cc62651b8cb77d999409
jaseongKim <jaseong4281@gmail.com>
2022-10-03 20:29:55 +0900

    [ refactor ] 카카오로그인시 비밀번호 변경 안보이기

--- src/components/mypage/MyPageList.jsx
+++ src/components/mypage/MyPageList.jsx
@@ -3,10 +3,14 @@ import styled from "styled-components";
 import MyPageItem from "./MyPageItem.jsx";
 
 export default function MyPageList() {
+  const isKako = localStorage.getItem("isKako");
+
   return (
     <MyPageContainer>
       <MyPageItem id={1} text={"닉네임 변경"} />
-      <MyPageItem id={2} text={"비밀번호 변경"} />
+      <div style={{ display: `${isKako === true ? "none" : "block"}` }}>
+        <MyPageItem id={2} text={"비밀번호 변경"} />
+      </div>
       <MyPageItem id={3} text={"로그아웃"} />
     </MyPageContainer>
   );
